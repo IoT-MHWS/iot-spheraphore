4ac52453b1682fe11ea58dd2282adda885e39e77
niqzart <qwert45hi@yandex.ru>
2023-06-10 16:04:19 +0300

    init: setup basic mongo database

---
+++ backend/app/config.py
@@ -0,0 +1,11 @@
+from os import getenv
+from pathlib import Path
+
+from motor.motor_asyncio import AsyncIOMotorClient  # type: ignore
+from odmantic import AIOEngine
+
+file_directory: Path = Path.cwd()
+
+mongodb_url: str = getenv("MONGODB_URL", "mongodb://root:example@localhost:27017/")
+client: AsyncIOMotorClient = AsyncIOMotorClient(mongodb_url)
+engine: AIOEngine = AIOEngine(client=client, database="videos")
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,26 @@
+from contextlib import asynccontextmanager
+from typing import AsyncIterator
+
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+from app.config import engine
+
+
+@asynccontextmanager
+async def lifespan(_: FastAPI) -> AsyncIterator[None]:
+    await engine.configure_database(
+        [],
+        update_existing_indexes=True,
+    )
+
+    yield
+
+    pass
+
+
+# noinspection PyTypeChecker
+app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
