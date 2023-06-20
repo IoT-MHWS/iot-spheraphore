5f8074316ffd9a7364a1fb89f15de56cbf453552
niqzart <qwert45hi@yandex.ru>
2023-06-20 22:02:30 +0300

    feat: admin method for updating cells

--- backend/app/routes/cells_mub.py
+++ backend/app/routes/cells_mub.py
@@ -1,8 +1,12 @@
-from fastapi import APIRouter
+from typing import Optional
+
+from fastapi import APIRouter, HTTPException
+from odmantic import ObjectId
 from pydantic import BaseModel
+from starlette.status import HTTP_404_NOT_FOUND
 
 from app.common.config import engine
-from app.models.cells_db import Cell
+from app.models.cells_db import Cell, ClimateMode, DeviceTypes, LightMode, Subject
 
 router = APIRouter(prefix="/admin/cells")
 
@@ -15,5 +19,27 @@ class CellInput(BaseModel):
 @router.post("")
 async def create_cell(data: CellInput) -> Cell:
     cell: Cell = Cell(**data.dict())
+    await engine.save(cell)
+    return cell
+
+
+class CellUpdate(BaseModel):
+    temperature: Optional[float] = None
+    climate_mode: Optional[ClimateMode] = None
+
+    illumination: Optional[float] = None
+    light_mode: Optional[LightMode] = None
+
+    subjects: Optional[list[Subject]] = None
+
+    devises: Optional[list[DeviceTypes]] = None
+
+
+@router.put("/<cell_id>")
+async def update_cell(data: CellUpdate, cell_id: ObjectId) -> Cell:
+    cell = await engine.find_one(Cell, Cell.id == cell_id)
+    if cell is None:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND)
+    cell.update(data.dict())
     await engine.save(cell)
     return cell
